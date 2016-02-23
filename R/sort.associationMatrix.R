3caf9ab4227e31da50d7437d2606be31ed2ee93d
Matherion <gjalt-jorn@behaviorchange.eu>
2016-02-23 14:30:47 +0100

    Corrected a minor bug in sort.associationMatrix
    Specifically, sorting an 'n X 1 matrix' simplified it into a vector -
    now it's converted into a matrix again.

--- R/sort.associationMatrix.R
+++ R/sort.associationMatrix.R
@@ -6,7 +6,7 @@ sort.associationMatrix <- function(x, decreasing = TRUE, byColumn = 1, ...) {
          "for symmetrical association matrices, sorting would not make much ",
          "sense.");
   }
-  
+
   ### First generate a numeric table with effectsizes and one with their types
   res <- x;
   res$intermediate$sorting <- list();
@@ -37,9 +37,19 @@ sort.associationMatrix <- function(x, decreasing = TRUE, byColumn = 1, ...) {
     
   res$output$matrix.raw <- res$output$matrix;
   
-  res$output$matrix$es <- res$output$matrix$es[res$intermediate$sorting$order, ];
-  res$output$matrix$ci <- res$output$matrix$ci[res$intermediate$sorting$order, ];
-  res$output$matrix$full <- res$output$matrix$full[res$intermediate$sorting$orderForFull, ];
+  ### Some measures to make sure we actually get a matrix back - apparently,
+  ### reordering a matrix also simplifies it to a vector :-/
+  nCol <- ncol(res$output$matrix.raw$es);
+  colNames <- colnames(res$output$matrix.raw$es);
+  rowNames <- rownames(res$output$matrix.raw$es)[res$intermediate$sorting$order];
+  rowNamesForFull <- rownames(res$output$matrix.raw$full)[res$intermediate$sorting$orderForFull];
+
+  res$output$matrix$es <- matrix(res$output$matrix$es[res$intermediate$sorting$order, ],
+                                 ncol=nCol, dimnames=list(rowNames, colNames));
+  res$output$matrix$ci <- matrix(res$output$matrix$ci[res$intermediate$sorting$order, ],
+                                 ncol=nCol, dimnames=list(rowNames, colNames));
+  res$output$matrix$full <- matrix(res$output$matrix$full[res$intermediate$sorting$orderForFull, ],
+                                   ncol=nCol, dimnames=list(rowNamesForFull, colNames));
 
   return(res);
   
