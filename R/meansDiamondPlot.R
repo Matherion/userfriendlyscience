36f4d02ab2ab8e9ad705f6b129891bd972cfc675
Gjalt-Jorn Peters <gjalt-jorn@greatergood.eu>
2017-01-23 14:30:56 +0100

    meansDiamondPlot: now using varsToDiamondPlotDf for generating dataframe to use for the diamondplots

--- R/meansDiamondPlot.R
+++ R/meansDiamondPlot.R
@@ -11,32 +11,40 @@ meansDiamondPlot <- function(dat, items = NULL, labels = NULL,
   res <- list();
   res$intermediate <- list();
   
-  if (is.null(items)) items <- names(dat);
-  if (is.null(labels)) labels <- items;
+  res$intermediate$dat <- varsToDiamondPlotDf(dat, items = items,
+                                              labels = labels,
+                                              decreasing=NULL,
+                                              conf.level=conf.level);
   
-  res$intermediate$dat <- data.frame(t(sapply(dat[, items], function(x) {
-    x <- na.omit(x);
-    ci <- meanConfInt(x, conf.level=conf.level)$output$ci;
-    return(data.frame(lo = ci[1], mean = mean(x), hi = ci[2]));
-  })));
+  sortedByMean <- attr(res$intermediate$dat, 'sortedByMean');
   
-  res$intermediate$dat$label <- labels;
-  res$intermediate$dat$rownr <- 1:nrow(res$intermediate$dat);
-  res$intermediate$dat$constant <- 1;
-
-  if (!is.null(decreasing)) {
-    ### Invert 'decreasing' because ggplot plots the lowest/first value first (near the origin).
-    ### So a decreasing sort would normally result in higher means being displayed LOWER in
-    ### the plot, which is counter-intuitive, hence the inversion.
-    sortedByMean <- order(unlist(res$intermediate$dat$mean), decreasing=!decreasing);
-    res$intermediate$dat <- res$intermediate$dat[sortedByMean, ];
-    labels <- labels[sortedByMean];
-  } else {
-    ### sortedByMean is used later on to organise the raw data; therefore, this should
-    ### reflect the order of the variables on the Y axis regardless of whether they're
-    ### reorganised
-    sortedByMean <- 1:length(labels);
-  }
+  # 
+  # if (is.null(items)) items <- names(dat);
+  # if (is.null(labels)) labels <- items;
+  # 
+  # res$intermediate$dat <- data.frame(t(sapply(dat[, items], function(x) {
+  #   x <- na.omit(x);
+  #   ci <- meanConfInt(x, conf.level=conf.level)$output$ci;
+  #   return(data.frame(lo = ci[1], mean = mean(x), hi = ci[2]));
+  # })));
+  # 
+  # res$intermediate$dat$label <- labels;
+  # res$intermediate$dat$rownr <- 1:nrow(res$intermediate$dat);
+  # res$intermediate$dat$constant <- 1;
+  # 
+  # if (!is.null(decreasing)) {
+  #   ### Invert 'decreasing' because ggplot plots the lowest/first value first (near the origin).
+  #   ### So a decreasing sort would normally result in higher means being displayed LOWER in
+  #   ### the plot, which is counter-intuitive, hence the inversion.
+  #   sortedByMean <- order(unlist(res$intermediate$dat$mean), decreasing=!decreasing);
+  #   res$intermediate$dat <- res$intermediate$dat[sortedByMean, ];
+  #   labels <- labels[sortedByMean];
+  # } else {
+  #   ### sortedByMean is used later on to organise the raw data; therefore, this should
+  #   ### reflect the order of the variables on the Y axis regardless of whether they're
+  #   ### reorganised
+  #   sortedByMean <- 1:length(labels);
+  # }
 
   plot <- diamondPlot(res$intermediate$dat, ciCols=c('lo', 'mean', 'hi'),
                       yLabels = labels, colorCol=diamondColors, ...); 
