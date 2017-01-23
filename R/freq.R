c907419c3799158740e386279d0c41fb1c882b3a
Gjalt-Jorn Peters <gjalt-jorn@greatergood.eu>
2017-01-23 15:25:34 +0100

    freq: allowed supplying logical vectors again

--- R/freq.R
+++ R/freq.R
@@ -48,7 +48,7 @@ freq <- Frequency <- function(vector, digits = 1, nsmall=1, transposed=FALSE, ro
               intermediate = list(),
               output = list());
   
-  if (!(is.factor(vector) | is.numeric(vector) | is.character(vector))) {
+  if (!(is.factor(vector) | is.numeric(vector) | is.logical(vector) | is.character(vector))) {
     stop("Please provide a single vector in argument 'vector' ",
          "(you supplied an object of class '", class(vector),
          "'). Use 'frequencies' ",
