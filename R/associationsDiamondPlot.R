bd5dbaedf80d189ba80e3bb83a0872587f55145b
Gjalt-Jorn Peters <gjalt-jorn@greatergood.eu>
2017-02-11 14:09:42 +0100

    Introducing diamons plots for associations

---
+++ R/associationsDiamondPlot.R
@@ -0,0 +1,28 @@
+associationsDiamondPlot <- function(dat, covariates, criterion,
+                                    labels = NULL,
+                                    decreasing=NULL,
+                                    conf.level=.95,
+                                    diamondColors = NULL,
+                                    returnLayerOnly = FALSE,
+                                    esMetric = 'r',
+                                    ...) {
+
+  res <- list(input = as.list(environment()),
+              intermediate = list());
+
+  res$intermediate$dat <- associationsToDiamondPlotDf(dat, covariates = covariates,
+                                                      criterion = criterion,
+                                                      labels = labels,
+                                                      decreasing=decreasing,
+                                                      conf.level=conf.level,
+                                                      esMetric = esMetric);
+  
+  ### Get labels from this dataframe, because they may have been sorted
+  labels <- res$intermediate$dat$label;
+
+  return(diamondPlot(res$intermediate$dat, ciCols=c('lo', 'es', 'hi'),
+                     yLabels = labels, colorCol=diamondColors,
+                     returnLayerOnly = returnLayerOnly, ...));
+  
+}
+                                
---
+++ R/associationsToDiamondPlotDf.R
@@ -0,0 +1,54 @@
+associationsToDiamondPlotDf <- function(dat, covariates, criterion,
+                                        labels = NULL,
+                                        decreasing=NULL,
+                                        conf.level=.95,
+                                        esMetric = 'r') {
+
+  if (is.null(labels)) labels <- covariates;
+
+  assocMatrix <- associationMatrix(dat, x=covariates, y=criterion);
+  
+  resDf <- data.frame(lo = as.numeric(assocMatrix$output$raw$ci.lo),
+                      es = as.numeric(assocMatrix$output$raw$es),
+                      hi = as.numeric(assocMatrix$output$raw$ci.hi));
+
+  if (esMetric == 'r') {
+    resDf <- data.frame(matrix(sapply(1:length(covariates), function(i) {
+      if (assocMatrix$output$raw$esType[i] == 'd') {
+        return(convert.d.to.r(resDf[i, ]));
+      } else if ((assocMatrix$output$raw$esType[i] == 'etasq') ||
+                 (assocMatrix$output$raw$esType[i] == 'omegasq')) {
+        return(sqrt(resDf[i, ]));
+      } else {
+        return(resDf[i, ]);
+      }
+    }), ncol=3, byrow=TRUE));
+  } else {
+    stop("No other effect size metrics implemented yet!");
+  }
+
+  names(resDf) <- c('lo', 'es', 'hi');
+  resDf$label <- labels;
+  resDf$rownr <- 1:nrow(resDf);
+  resDf$constant <- 1;
+  
+  if (!is.null(decreasing)) {
+    ### Invert 'decreasing' because ggplot plots the lowest/first value first (near the origin).
+    ### So a decreasing sort would normally result in higher means being displayed LOWER in
+    ### the plot, which is counter-intuitive, hence the inversion.
+    sortedByMean <- order(unlist(resDf$es), decreasing=!decreasing);
+    resDf <- resDf[sortedByMean, ];
+    labels <- labels[sortedByMean];
+  } else {
+    ### sortedByMean is used later on to organise the raw data; therefore, this should
+    ### reflect the order of the variables on the Y axis regardless of whether they're
+    ### reorganised
+    sortedByMean <- 1:length(labels);
+  }
+  
+  ### Return this vector as attribute to use in meansDiamondPlot
+  attr(resDf, 'sortedByMean') <- sortedByMean;
+
+  return(resDf);
+  
+}
