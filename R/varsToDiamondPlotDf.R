18ab50ff70a086dbf54c462c495c815686997f1d
Gjalt-Jorn Peters <gjalt-jorn@greatergood.eu>
2017-03-01 15:07:31 +0100

    Prevented dataframe from becoming vector

--- R/varsToDiamondPlotDf.R
+++ R/varsToDiamondPlotDf.R
@@ -9,7 +9,7 @@ varsToDiamondPlotDf <- function(dat, items = NULL, labels = NULL,
   }
   if (is.null(labels)) labels <- items;
 
-  resDf <- data.frame(t(sapply(dat[, items], function(x) {
+  resDf <- data.frame(t(sapply(dat[, items, drop=FALSE], function(x) {
     x <- na.omit(x);
     ci <- meanConfInt(x, conf.level=conf.level)$output$ci;
     return(data.frame(lo = ci[1], mean = mean(x), hi = ci[2]));
@@ -18,7 +18,7 @@ varsToDiamondPlotDf <- function(dat, items = NULL, labels = NULL,
   resDf$label <- labels;
   resDf$rownr <- 1:nrow(resDf);
   resDf$constant <- 1;
-  
+
   if (!is.null(decreasing)) {
     ### Invert 'decreasing' because ggplot plots the lowest/first value first (near the origin).
     ### So a decreasing sort would normally result in higher means being displayed LOWER in
@@ -32,10 +32,10 @@ varsToDiamondPlotDf <- function(dat, items = NULL, labels = NULL,
     ### reorganised
     sortedByMean <- 1:length(labels);
   }
-  
+
   ### Return this vector as attribute to use in meansDiamondPlot
   attr(resDf, 'sortedByMean') <- sortedByMean;
 
   return(resDf);
-  
+
 }
