c9ed7cb17c24ee9e07a7f1d809b086a29ea23932
Gjalt-Jorn Peters <gjalt-jorn@greatergood.eu>
2017-03-12 13:56:59 +0100

    More fine-grained control to sizes

--- R/rawDataDiamondLayer.R
+++ R/rawDataDiamondLayer.R
@@ -3,22 +3,23 @@ rawDataDiamondLayer <- function(dat, items = NULL, itemOrder = 1:length(items),
                                 dataColor = "#444444",
                                 jitterWidth = .5,
                                 jitterHeight = .4,
+                                size=2,
                                 ...) {
 
   rawData <- na.omit(data.frame(value = unlist(dat[, items[itemOrder]]),
                                 labels = rep(1:length(items),
                                              each=nrow(dat))));
-  
+
   rawDataLayer <- geom_jitter(data=rawData,
                               mapping=aes_string(x='value', y='labels'),
-                              size = 2.5,
+                              size = size,
                               color = dataColor,
                               alpha = dataAlpha,
                               stroke = 0,
                               width=jitterWidth,
                               height=jitterHeight,
-                              ...);  
-  
+                              ...);
+
   return(rawDataLayer);
-  
+
 }
