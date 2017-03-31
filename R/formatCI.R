016ca178764ddaf4969c7ae70c52fbf831197cfd
Gjalt-Jorn Peters <gjalt-jorn@greatergood.eu>
2017-03-31 12:18:17 +0200

    Minor debug

--- R/formatCI.R
+++ R/formatCI.R
@@ -1,7 +1,7 @@
 formatCI <- function(ci, sep='; ', prefix='[', suffix=']', digits=2, noZero=FALSE) {
   if (noZero) {
-    return(paste0(prefix, paste0(noZero(round(ci, 2)), collapse=sep), suffix));
+    return(paste0(prefix, paste0(noZero(round(ci, digits)), collapse=sep), suffix));
   } else {
-    return(paste0(prefix, paste0(round(ci, 2), collapse=sep), suffix));
+    return(paste0(prefix, paste0(round(ci, digits), collapse=sep), suffix));
   }
 }
