87d4fbde471be2ded84617037a25a4f087b9b91a
Gjalt-Jorn Peters <gjalt-jorn@greatergood.eu>
2017-04-14 10:01:02 +0200

    Added confidenceCurve function

---
+++ R/confidenceCurve.R
@@ -0,0 +1,63 @@
+confidenceCurve <- function(metric = 'd', value = .5, n = 128,
+                            conf.level=NULL,
+                            wRange = c(.05, 1),
+                            confRange = c(.0001, .9999),
+                            steps=1000,
+                            theme=theme_bw()) {
+  ### Check arguments
+  if (is.null(conf.level) && is.null(n)) {
+    stop("Arguments 'conf.level' and 'n' cannot both be NULL!");
+  }
+  if (is.null(value)) {
+    stop("You must always specify a value for your metric.");
+  }
+  if (tolower(metric) == 'd') {
+    if (is.null(n)) {
+      ### So, sample sizes on the y axis, and values on the x axis.
+      ### So we need the values (bounds) corresponding to different
+      ### widths (which then correspond to sample sizes).
+      minN <- pwr.cohensdCI(d=value, w=max(wRange));
+      maxN <- pwr.cohensdCI(d=value, w=min(wRange));
+      df1 <- df2 <- data.frame(n = seq(minN, maxN, length.out=steps));
+      confInts <- cohensdCI(d=value,
+                            n=seq(minN, maxN, length.out=steps),
+                            conf.level=conf.level);
+      df1 <- cbind(df1, confInts[, 1]);
+      df2 <- cbind(df2, confInts[, 2]);
+      names(df1) <- names(df2) <- c('n', 'd');
+      plot <- ggplot() +
+        geom_line(data=df1, aes_string(x='d', y='n')) +
+        geom_line(data=df2, aes_string(x='d', y='n')) +
+        ggtitle(paste0("Inverse Confidence Curve for ",
+                       metric, " = ", value, " and conf.level = ", conf.level));
+      
+    } else {
+      ### So, confidence is on the y axis, and values on the x axis.
+      ### So we need to the confidence interval bounds for the given
+      ### sample size, for different confidence levels.
+      minConf <- min(confRange);
+      maxConf <- max(confRange);
+      df1 <- df2 <- data.frame(confidence = seq(minConf, maxConf, length.out=steps));
+      
+      confInts <- matrix(unlist(lapply(df1$confidence,
+                                       cohensdCI,
+                                       d=value,
+                                       n=n)), ncol=2, byrow=TRUE);
+      df1 <- cbind(df1, confInts[, 1]);
+      df2 <- cbind(df2, confInts[, 2]);
+      names(df1) <- names(df2) <- c('confidence', 'd');
+      plot <- ggplot() +
+        geom_line(data=df1, aes_string(x='d', y='confidence')) +
+        geom_line(data=df2, aes_string(x='d', y='confidence')) +
+        ggtitle(paste0("Confidence Curve for ",
+                       metric, " = ", value, " and n = ", n));
+    }
+  } else {
+    stop("Sorry, metric '", metric, "' is not implemented.");
+  }
+  plot <- plot + theme;
+  return(plot);
+}
+
+require(ggplot2);
+print(confidenceCurve());
