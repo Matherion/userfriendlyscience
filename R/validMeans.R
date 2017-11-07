3afd78e44e17ae95a5b5b1250f08e4eb8e6ac77c
Gjalt-Jorn Peters <gjalt-jorn@greatergood.eu>
2017-11-07 10:17:37 +0100

    Added validMeans and validSums

---
+++ R/validMeans.R
@@ -0,0 +1,29 @@
+validMeans <- function(...,
+                       requiredValidValues = .8,
+                       returnIfInvalid = NA,
+                       silent = FALSE) {
+  dat <- list(...);
+  if ((length(dat) == 1) && is.data.frame(dat[[1]])) {
+    dat <- dat[[1]];
+  } else if (length(unique(lapply(dat, length)))==1) {
+    dat <- as.data.frame(dat);
+  } else {
+    stop("The vectors you provided do not have equal lengths! Either provide a dataframe or vectors of the same length");
+  }
+  if (!is.numeric(requiredValidValues)) {
+    stop("Argument 'requiredValidValues' must be numeric, but is ", class(requiredValidValues), ".");
+  }
+  if (requiredValidValues < 1) {
+    requiredValidValuesPercentages <- requiredValidValues;
+    requiredValidValues <- ceiling(requiredValidValues * ncol(dat));
+    if (!silent) {
+      cat0("Argument 'requiredValidValues' was set to a proportion (",
+           requiredValidValuesPercentages, "), so only computing a mean for cases ",
+           "where that proportion of variables (i.e. ",
+           100 * requiredValidValuesPercentages,
+           "%, or ", requiredValidValues, " variables) have valid values.\n");
+    }
+  }
+  nrOfValidValues <- rowSums(!is.na(dat)) >= requiredValidValues;
+  return(ifelse(nrOfValidValues, rowMeans(dat), returnIfInvalid));
+}
