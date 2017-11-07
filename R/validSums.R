dd8d153b8303920e4cdd877348bff5e1a465aaf8
Gjalt-Jorn Peters <gjalt-jorn@greatergood.eu>
2017-11-07 10:29:15 +0100

    Minor improvements and added function export to NAMESPACE

--- R/validSums.R
+++ R/validSums.R
@@ -1,5 +1,5 @@
 validSums <- function(...,
-                       requiredValidValues = .8,
+                       requiredValidValues = 0,
                        returnIfInvalid = NA,
                        silent = FALSE) {
   dat <- list(...);
@@ -8,22 +8,25 @@ validSums <- function(...,
   } else if (length(unique(lapply(dat, length)))==1) {
     dat <- as.data.frame(dat);
   } else {
-    stop("The vectors you provided do not have equal lengths! Either provide a dataframe or vectors of the same length");
+    stop("The vectors you provided do not have equal lengths! Either provide a dataframe or vectors of the same length.");
   }
-  if (!is.numeric(requiredValidValues)) {
-    stop("Argument 'requiredValidValues' must be numeric, but is ", class(requiredValidValues), ".");
-  }
-  if (requiredValidValues < 1) {
+  if (requiredValidValues == "all") {
+    requiredValidValues <- ncol(dat);
+  } else if (!is.numeric(requiredValidValues)) {
+    stop("Argument 'requiredValidValues' must be numeric or 'all', ",
+         "but it is not 'all' and has class ",
+         class(requiredValidValues), ".");
+  } else if (requiredValidValues < 1) {
     requiredValidValuesPercentages <- requiredValidValues;
     requiredValidValues <- ceiling(requiredValidValues * ncol(dat));
     if (!silent) {
       cat0("Argument 'requiredValidValues' was set to a proportion (",
-           requiredValidValuesPercentages, "), so only computing a sum for cases ",
+           requiredValidValuesPercentages, "), so only computing a mean for cases ",
            "where that proportion of variables (i.e. ",
            100 * requiredValidValuesPercentages,
            "%, or ", requiredValidValues, " variables) have valid values.\n");
     }
   }
   nrOfValidValues <- rowSums(!is.na(dat)) >= requiredValidValues;
-  return(ifelse(nrOfValidValues, rowSums(dat), returnIfInvalid));
+  return(ifelse(nrOfValidValues, rowSums(dat, na.rm=TRUE), returnIfInvalid));
 }
